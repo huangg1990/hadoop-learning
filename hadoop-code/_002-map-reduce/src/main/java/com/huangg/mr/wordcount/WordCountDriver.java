acb084408acce499b09f328f4f096d54dd28ac2c
huanggang <huanggang@fabrique.cn>
2023-09-17 22:11:06 +0800

    wordCount

---
+++ hadoop-code/_002-map-reduce/src/main/java/com/huangg/mr/wordcount/WordCountDriver.java
@@ -0,0 +1,47 @@
+package com.huangg.mr.wordcount;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class WordCountDriver {
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        // 1 获取Job对象
+        Job job = Job.getInstance(conf);
+
+        // 2 设置Jar存储位置
+        job.setJarByClass(WordCountDriver.class);
+
+        // 3 关联Map和Reduce类
+        job.setMapperClass(WordCountMapper.class);
+        job.setCombinerClass(WordCountReducer.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        // 4 设置Mapper阶段输出数据的Key和value类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        // 5 设置最终数据输出的key和value 类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // 6 设置输入路径和输出路径
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // 7 提交Job
+//        job.submit();
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+
+    }
+}
---
+++ hadoop-code/_002-map-reduce/src/main/java/com/huangg/mr/wordcount/WordCountMapper.java
@@ -0,0 +1,36 @@
+package com.huangg.mr.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * Map 阶段
+ * keyin 输入数据的KEY
+ * Value 输入数字的valeu
+ * keyout 输出数据的Key的类型
+ * valueout 输出的数据value类型
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private Text k = new Text();
+    private final static IntWritable v = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+            throws IOException, InterruptedException {
+        // 1 获取一行数据
+        String line = value.toString();
+        // 2 切分单词
+        String[] words = line.split(" ");
+        // 3 循环写入
+        for (String word : words) {
+            k.set(word);
+            context.write(k, v);
+        }
+
+    }
+}
